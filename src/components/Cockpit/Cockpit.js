0ca1b6eed868d5d0b85a879df1b5ac2ad300b0da
Barry McAuley <bamcauley@tycoint.com>
2020-08-03 16:08:44 +0100

    Refs

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,10 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import classes from './Cockpit.css';
 
 const Cockpit = (props) => {
+  const toggleBtnRef = useRef(null);
+
   useEffect(() => {
     console.log('[Cockpit.js] useEffect');
-    setTimeout(alert('Saved to DB', 1000));
+    toggleBtnRef.current.click();
+    // setTimeout(alert('Saved to DB', 1000));
     return () => {
       console.log('[Cockpit.js] cleanup work in useEffect');
     };
@@ -27,7 +30,7 @@ const Cockpit = (props) => {
     <div className={classes.Cockpit}>
       <h1>{props.title}</h1>
       <p className={assignedClasses.join(' ')}>This is really working!</p>
-      <button className={btnClass} onClick={props.clicked}>
+      <button ref={toggleBtnRef} className={btnClass} onClick={props.clicked}>
         Toggle Persons
       </button>
     </div>
--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -5,6 +5,15 @@ import withClass from '../../../hoc/withClass';
 import PropTypes from 'prop-types';
 
 class Person extends Component {
+  constructor(props) {
+    super(props);
+    this.inputElementRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.inputElementRef.current.focus();
+  }
+
   render() {
     console.log('[Person.js] rendering...');
     return (
@@ -14,6 +23,7 @@ class Person extends Component {
         </p>
         <input
           type="text"
+          ref={this.inputElementRef}
           onChange={this.props.changed}
           value={this.props.name}
         />
